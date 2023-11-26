bcab69107e18a69c598093fb8067c2b721ea1901
MrDionesalvi <dionesalvip@gmail.com>
2023-11-26 18:19:55 +0100

    Revert "PROVA BACKGROUND"
    This reverts commit 7a57ab18bc4e8898ed9bb58f76e89218dbb6b6bf.
    
    	modified:   lib/main.dart
    	modified:   pubspec.lock
    	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,6 @@ import 'package:BitTrans/pages/home.dart';
 import 'package:BitTrans/pages/onBoarding.dart';
 import 'package:BitTrans/prefrences.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:background_fetch/background_fetch.dart';
-
-// [Android-only] This "Headless Task" is run when the Android app is terminated with `enableHeadless: true`
-// Be sure to annotate your callback function to avoid issues in release mode on Flutter >= 3.3.0
-@pragma('vm:entry-point')
-void backgroundFetchHeadlessTask(HeadlessTask task) async {
-  String taskId = task.taskId;
-  bool isTimeout = task.timeout;
-  if (isTimeout) {
-    // This task has exceeded its allowed running-time.  
-    // You must stop what you're doing and immediately .finish(taskId)
-    print("[BackgroundFetch] Headless task timed-out: $taskId");
-    BackgroundFetch.finish(taskId);
-    return;
-  }  
-  print('[BackgroundFetch] Headless event received.');
-  // Do your work here...
-  BackgroundFetch.finish(taskId);
-}
-
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,44 +12,16 @@ Future main() async {
   final showHome = prefs.getBool('showHome') ?? false;
 
   runApp(MyApp(showHome: showHome));
-
-  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
-  BackgroundFetch.start();
 }
 
 class MyApp extends StatelessWidget {
   final bool showHome;
 
-
   const MyApp({
     Key? key,
     required this.showHome,
   }) : super(key: key);
-  
-  Future<void> initiPlatformState() async {
-    int status = await BackgroundFetch.configure(BackgroundFetchConfig(
-        minimumFetchInterval: 15,
-        stopOnTerminate: false,
-        enableHeadless: true,
-        requiresBatteryNotLow: false,
-        requiresCharging: false,
-        requiresStorageNotLow: false,
-        requiresDeviceIdle: false,
-        requiredNetworkType: NetworkType.NONE
-    ), (String taskId) async {  // <-- Event handler
-      // This is the fetch-event callback.
-      print("[BackgroundFetch] Event received $taskId");
 
-      // IMPORTANT:  You must signal completion of your task or the OS can punish your app
-      // for taking too long in the background.
-      BackgroundFetch.finish(taskId);
-    }, (String taskId) async {  // <-- Task timeout handler.
-      // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
-      print("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
-      BackgroundFetch.finish(taskId);
-    });
-    print('[BackgroundFetch] configure success: $status');   
-  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
